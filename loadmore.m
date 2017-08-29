8892bb4fa1ede9ac3d8e4b4bafaef7795a34eaa5
Tien Nguyen <tiennguyen@Tiens-MacBook-Pro.local>
2017-08-29 13:28:50 +0700

    loadmore

---
+++ loadmore.m
@@ -0,0 +1,13 @@
+-(void) tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if([indexPath row] == ((NSIndexPath*)[[tableView indexPathsForVisibleRows] lastObject]).row){
+        NSInteger lastSectionIndex = [tableView numberOfSections] - 1;
+        NSInteger lastRowIndex = [tableView numberOfRowsInSection:lastSectionIndex] - 1;
+        if ((indexPath.section == lastSectionIndex) && (indexPath.row == lastRowIndex)) {
+            // This is the last cell
+            if ([arrayBalanceList count]!=0) {
+                [self loadMore];
+            }
+        }
+    }
+}
